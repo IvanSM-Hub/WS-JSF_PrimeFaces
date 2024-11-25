4379c22cb6ab11cf19909408ca249d689889a929
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2024-11-25 15:08:47 +0100

    bug with the styles

--- JSF_PrimeFaces_App/src/main/java/com/jsfstarterproject/internationalization/MenuBean.java
+++ JSF_PrimeFaces_App/src/main/java/com/jsfstarterproject/internationalization/MenuBean.java
@@ -1,5 +1,8 @@
 package com.jsfstarterproject.internationalization;
 
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import org.primefaces.model.DashboardColumn;
@@ -8,24 +11,35 @@ import org.primefaces.model.DefaultDashboardColumn;
 import org.primefaces.model.DefaultDashboardModel;
 
 @Named
-public class MenuBean {
+@ViewScoped
+public class MenuBean implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	private DashboardModel model;
 	
 	public MenuBean() {
-        model = new DefaultDashboardModel();
+        
+		model = new DefaultDashboardModel();
+        
         DashboardColumn column1 = new DefaultDashboardColumn();
         DashboardColumn column2 = new DefaultDashboardColumn();
         DashboardColumn column3 = new DefaultDashboardColumn();
+        
         column1.addWidget("sports");
         column1.addWidget("viewreserves");
         column2.addWidget("lifestyle");
         column2.addWidget("stadistics");
         column3.addWidget("direct");
+        
         model.addColumn(column1);
         model.addColumn(column2);
         model.addColumn(column3);
-        System.out.println(column1);
+        
     }
+
+	public DashboardModel getModel() {
+		return model;
+	}
 	
 }
